056e60909566da39c3b77cc9de788a278f60edd4
salimtaiyo <ichigeki.ccCsc@hotmail.com>
2018-09-21 02:39:40 -0400

    finished the basic layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import  Recipe  from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes:Recipe[] = [
+    new Recipe('Test', 'Easiest dish ever', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/1200px-Good_Food_Display_-_NCI_Visuals_Online.jpg'),
     new Recipe('Test', 'Easiest dish ever', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/1200px-Good_Food_Display_-_NCI_Visuals_Online.jpg')
   ]
   constructor() { }
